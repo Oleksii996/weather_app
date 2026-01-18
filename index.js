4fae6ebf519219815816b9896e08161622943951
oleksii ponomarenko <alexponomarenko996@gmail.com>
2026-01-18 18:26:23 +0100

    udpate code

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const weatherDetails = document.querySelector(".weather-details");
 const error404 = document.querySelector(".not-found");
 const input = document.querySelector(".search-box input");
 
-// стартовий стан
+// початковий стан
 container.classList.add("collapsed");
 
-// робоча головна функція
+// функція пошуку
 function searchWeather() {
   const APIKey = "efd960ae82bc2e0fde37919c7b718a74";
   const city = input.value.trim();
@@ -18,7 +18,6 @@ function searchWeather() {
   if (!city) return;
 
   clearBtn.style.display = "block";
-  searchBtn.style.display = "none";
 
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`,
@@ -26,65 +25,61 @@ function searchWeather() {
     .then((response) => response.json())
     .then((json) => {
       if (json.cod === "404") {
-        container.style.height = "fit-content";
-        weatherBox.style.display = "none";
-        weatherDetails.style.display = "none";
-        error404.style.display = "block";
-        error404.classList.add("fadeIn");
+        showError();
         return;
-      }
-
-      error404.style.display = "none";
-      error404.classList.remove("fadeIn");
-
-      const image = document.querySelector(".weather-box img");
-      const temperature = document.querySelector(".weather-box .temperature");
-      const description = document.querySelector(".weather-box .description");
-      const humidity = document.querySelector(
-        ".weather-details .humidity span",
-      );
-      const wind = document.querySelector(".weather-details .wind span");
-
-      switch (json.weather[0].main) {
-        case "Clear":
-          image.src = "img/clear.svg";
-          break;
-
-        case "Rain":
-          image.src = "img/rain.png";
-          break;
-
-        case "Snow":
-          image.src = "img/snow.png";
-          break;
-
-        case "Clouds":
-          image.src = "img/cloud.svg";
-          break;
-
-        case "Haze":
-          image.src = "img/mist.png";
-          break;
-
-        default:
-          image.src = "";
-      }
-
-      temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
-      description.innerHTML = `${json.weather[0].description}`;
-      humidity.innerHTML = `${json.main.humidity}%`;
-      wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
-
-      weatherBox.style.display = "";
-      weatherDetails.style.display = "";
-
-      weatherBox.classList.add("fadeIn");
-      weatherDetails.classList.add("fadeIn");
-
-      expandContainer();
+      } else showWeather(json);
     });
 }
+// функція вдалого результату
+function showWeather(json) {
+  error404.style.display = "none";
+  error404.classList.remove("fadeIn");
 
+  const image = document.querySelector(".weather-box img");
+  const temperature = document.querySelector(".weather-box .temperature");
+  const description = document.querySelector(".weather-box .description");
+  const humidity = document.querySelector(".weather-details .humidity span");
+  const wind = document.querySelector(".weather-details .wind span");
+
+  switch (json.weather[0].main) {
+    case "Clear":
+      image.src = "img/clear.svg";
+      break;
+
+    case "Rain":
+      image.src = "img/rain.png";
+      break;
+
+    case "Snow":
+      image.src = "img/snow.png";
+      break;
+
+    case "Clouds":
+      image.src = "img/cloud.svg";
+      break;
+
+    case "Haze":
+      image.src = "img/mist.png";
+      break;
+
+    default:
+      image.src = "";
+  }
+
+  temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+  description.innerHTML = `${json.weather[0].description}`;
+  humidity.innerHTML = `${json.main.humidity}%`;
+  wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+  weatherBox.style.display = "block";
+  weatherDetails.style.display = "flex";
+
+  weatherBox.classList.add("fadeIn");
+  weatherDetails.classList.add("fadeIn");
+
+  expandContainer();
+}
+//функція помилки
 function showError() {
   weatherBox.style.display = "none";
   weatherDetails.style.display = "none";
@@ -99,32 +94,38 @@ function expandContainer() {
   container.classList.remove("collapsed");
   container.classList.add("expanded");
 }
-
 function collapseContainer() {
   container.classList.remove("expanded");
   container.classList.add("collapsed");
 }
 
-// події
+// подія кліком
 searchBtn.addEventListener("click", searchWeather);
 
-// викликаємо по натисканню Enter
+// подія Enter
 input.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     searchWeather();
   }
 });
 
-// викликаємо мишею
+// подія очищення
 clearBtn.addEventListener("click", () => {
   input.value = "";
   clearBtn.style.display = "none";
   searchBtn.style.display = "block";
 
-  // ховаємо результати
-  weatherBox.style.display = "none";
-  weatherDetails.style.display = "none";
-  error404.style.display = "none";
-
   collapseContainer();
+
+  const onTransitionEnd = (e) => {
+    if (e.propertyName !== "max-height") return;
+
+    weatherBox.style.display = "none";
+    weatherDetails.style.display = "none";
+    error404.style.display = "none";
+
+    container.removeEventListener("transitionend", onTransitionEnd);
+  };
+
+  container.addEventListener("transitionend", onTransitionEnd);
 });
